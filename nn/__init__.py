d4f9f671c8f3d177f5d6759d056ec245380a75f0
piyush2896 <piyushmalhotra28@gmail.com>
2018-07-20 11:23:05 +0530

    First Commit

---
+++ nn/__init__.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def relu(x):
+    return np.maximum(x, 0)
+
+def leaky_rely(x, alpha=0.01):
+    nonlin = relu(x)
+    nonlin[nonlin==0] = alpha * x[nonlin == 0]
+    return nonlin
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(x))
+
+def softmax(x):
+    e_x = np.exp(x - np.max(x))
+    return e_x / e_x.sum()
